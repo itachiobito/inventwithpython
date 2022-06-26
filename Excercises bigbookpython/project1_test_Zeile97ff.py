c1fc62c04c58fc3aaad6a4eb0d4db3fbd4471fc8
itachiobito <zitruspresse@gmx.de>
2022-06-26 20:24:53 +0200

    Mechanismus if __name__ == __main__

--- Excercises bigbookpython/project1_test_Zeile97ff.py
+++ Excercises bigbookpython/project1_test_Zeile97ff.py
@@ -1,5 +1,10 @@
+
+"""
+https://www.data-science-architect.de/__name____main__/
+In diesem Artikel wird die if Schleife mit der Systemvariablen name beschrieben
+"""
+
 from project1 import getSecretNum
 
-
 print('Wert von __name__ in project1_test_Zeile97ff.py:', __name__)
-print(getSecretNum())
+print(getSecretNum())  # Aufruf der Funktion aus dem Modul project1
